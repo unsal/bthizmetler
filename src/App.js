a3dccfb47b29b120950a42b1eb089ec6ced24fc6
unsal <unsal@unsal.org>
2018-05-05 18:21:53 +0300

    ProjelreCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import BTMenu from './menu';
+import BTMenu from './menu/menu';
 import { Header, Container, Segment, Image } from "semantic-ui-react";
 
 
 
 class App extends Component {
   render() {
-    return <Segment basic>
+    return <Segment tertiary basic>
               <Container textAlign="justified">
                   <Header as="h1"><Image src='/img/_logo.png' size='huge' />
                       <Header.Content> Bilgi Teknolojileri </Header.Content>
